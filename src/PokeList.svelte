1a6f1a03c6c1c89be19e0979238ac85603866f23
adam-kuhn <adam.kuhn@smartly.io>
2021-11-04 12:33:47 +0200

    Adding loading icon - use a delay to simulate the loader

--- src/PokeList.svelte
+++ src/PokeList.svelte
@@ -1,14 +1,18 @@
 <script>
+  import {Jellyfish} from 'svelte-loading-spinners'
     let page = 1;
     let pokemonList = [];
     let pages = []
     let lastPage;
     let pokemonPerPage = 20;
     let error = false;
+    let loading = true;
     $: {
       const limit = pokemonPerPage;
       const offSet = limit * (page - 1);
-      fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offSet}`, {method: "GET"})
+      loading = true;
+      setTimeout(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offSet}`, {method: "GET"})
         .then(response => (response.json()))
         .then(data => {
           lastPage = Math.ceil(data.count / limit);
@@ -16,7 +20,11 @@
         })
         .catch((err) => {
           console.error("Something went wrong", err);  
+        })
+        .finally(() => {
+          loading = false
         });
+      }, 3000)
     }
     $: {
       pages = calculatePages(lastPage)
@@ -55,28 +63,32 @@
 </style>
 
 <h1>Pokemon</h1>
-<ul>
-	{#each pokemonList as pokemon}
-	<li>{pokemon.name}</li>
-	{/each}
-</ul>
-<div class='footer'>
-  <div class='row-content'>
-    <label for='poke-per-page'># per page: {pokemonPerPage}</label>
-    <input id='poke-per-page' value={pokemonPerPage} on:change={handleChange} type='text' />
-    {#if error}
-    <p>Please input a number</p>
-    {/if}
-    <label for="page-selection">Go to page:</label>
-    <select bind:value={page}>
-      {#each pages as page}
-        <option value={page}>{page}</option>
-      {/each}
-    </select>
+{#if loading}
+  <Jellyfish />
+  {:else}
+  <ul>
+    {#each pokemonList as pokemon}
+    <li>{pokemon.name}</li>
+    {/each}
+  </ul>
+  <div class='footer'>
+    <div class='row-content'>
+      <label for='poke-per-page'># per page: {pokemonPerPage}</label>
+      <input id='poke-per-page' value={pokemonPerPage} on:change={handleChange} type='text' />
+      {#if error}
+      <p>Please input a number</p>
+      {/if}
+      <label for="page-selection">Go to page:</label>
+      <select bind:value={page}>
+        {#each pages as page}
+          <option value={page}>{page}</option>
+        {/each}
+      </select>
+    </div>
+    <div class='row-content'>  
+      <button on:click={() => page--} disabled={page === 1}>Prev</button>
+      <p>Current Page: {page}</p>
+      <button on:click={() => page++} disabled={page === lastPage}>Next page</button>
+    </div>
   </div>
-  <div class='row-content'>  
-    <button on:click={() => page--} disabled={page === 1}>Prev</button>
-    <p>Current Page: {page}</p>
-    <button on:click={() => page++} disabled={page === lastPage}>Next page</button>
-  </div>
-</div>
+{/if}
